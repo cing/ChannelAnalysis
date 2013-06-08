d294ab437195488647d7ed2e1c01378dd27261cb
Christopher Ing <ing.chris@gmail.com>
2013-06-08 01:49:50 -0400

    Added additional regex analysis

--- InitCoordAnalysis.py
+++ InitCoordAnalysis.py
@@ -21,7 +21,7 @@ def main(args):
                                           sort_col=args.sort_col,
                                           add_time=args.add_time)
 
-    #write_columns(data_f, outfile="test")
+    #write_columns(data_f, outfile=pre_prefix+"test")
 
     # Ion occupancy in the channel (governed by the MDAnalysis selection radii)
     print "Channel Occupancy"
@@ -30,7 +30,7 @@ def main(args):
                       prefix=pre_prefix+"chanocc")
 
     write_occ_vs_time(data_f, num_cols=args.num_cols,
-                      prefix=pre_prefix+"chanocc")
+                      traj_col=args.traj_col, prefix=pre_prefix+"chanocc")
 
     # Same thing but now we pass the SF column list.
     print "SF Occupancy using columns", args.sf_col
@@ -38,7 +38,7 @@ def main(args):
                       traj_col=args.traj_col, sf_col=args.sf_col,
                       prefix=pre_prefix+"sfocc")
 
-    write_occ_vs_time(data_f, num_cols=args.num_cols,
+    write_occ_vs_time(data_f, num_cols=args.num_cols, traj_col=args.traj_col,
                       sf_col=args.sf_col, prefix=pre_prefix+"sfocc")
 
     print "Writing multiple 1D histograms for ion coordination integers"
@@ -81,12 +81,47 @@ def main(args):
                            sort_col=args.sort_col,
                            prefix=pre_prefix+"1dhisto")
 
+    print "State Transition Counting"
+    data_state_trans = state_transitions(data_f_padded, data_f_regex,
+                            time_col=args.time_col,
+                            time_increment=args.time_increment,
+                            traj_col=args.traj_col)
+
+    print "Computing Regex State Occupancies for macrostate graph"
+    data_f_occupancy = regex_counter(data_f_padded, data_f_regex,
+                        num_ions_map=args.num_ions_map,
+                        traj_col=args.traj_col)
+    # We want to extract only the mean occupancies in percent (2nd last entry)
+    data_f_mean_pop = data_f_occupancy[-2][1:]
+
+    print "State Transition Graph Building and Writing"
+    #print data_state_trans
+    state_draw_map = [(0.0, 0),(1.0, 0),(2.0, 0),(3.0, 0),
+                          (0.5,-1),(1.5,-1),(2.5,-1),
+                               (1.0,-2),(2.0,-2),(3.0,-2),
+                                   (1.5,-3),(2.5,-3),
+                                        (2.0,-4)]
+
+    data_state_trans_graph = build_transition_graph(data_state_trans,
+                                                    state_draw_map,
+                                                    pop_map=data_f_mean_pop)
+
+    write_transitions_graph(data_state_trans_graph,
+                            outfile=pre_prefix+"graph_regex_macro.pdf")
+
+    print "State Transition w/ Intermediate Counting"
+    print state_intermediate_transitions(data_f_padded, data_f_regex,
+                                         time_col=args.time_col,
+                                         time_increment=args.time_increment,
+                                         traj_col=args.traj_col)
+
 
 if __name__ == '__main__':
     parser = ArgumentParser(
     description='This script parses input columnular ASCII data\
     and makes it nice and pretty for subsequent analysis.')
 
+    # These arguments are required for input processing and are required
     parser.add_argument(
     '-f', dest='filenames', type=str, nargs="+", required=True,
     help='a filename of coordination data from MDAnalysis trajectory data')
@@ -106,17 +141,13 @@ if __name__ == '__main__':
     '-t', dest='traj_col', type=int, default=11,
     help='a zero inclusive column number that contains the run number')
     parser.add_argument(
-    '-o', dest='outfile', type=str, default=None,
-    help='the file to output the sorted padding output of all input files')
-    parser.add_argument(
     '--addtime', dest='add_time', action="store_true", default=False,
     help='an optional argument to add time columns to each ion grouping')
+
+    # The following is used for 1D coordination histograms
     parser.add_argument(
     '-coord', dest='coord_cols', type=int, nargs="+", default=[4,5,6,7,8,9,10],
     help='all the columns with coordination counts')
-    parser.add_argument(
-    '-n', dest='num_ions_map', type=int, nargs="+", default=[1,2,2,3,0],
-    help='list of integer ion counts in SF for each regex value + 1 for extra')
 
     # The following arguments are used for regex state stream processing
     parser.add_argument(
@@ -128,6 +159,18 @@ if __name__ == '__main__':
     parser.add_argument(
     '-sf', dest='sf_col', type=int, nargs="+", default=[5,6],
     help='the coordination integer columns that define the selectivity filter')
+    parser.add_argument(
+    '-n', dest='num_ions_map', type=int, nargs="+", default=[1,2,2,3,0],
+    help='list of integer ion counts in SF for each regex value + 1 for extra')
+
+    # The following arguments are used for regex state transition processing
+    parser.add_argument(
+    '-timecol', dest='time_col', type=int, default=0,
+    help='a zero inclusive column number with the frame/timestep number')
+    parser.add_argument(
+    '-dt', dest='time_increment', type=int, default=1,
+    help='the difference in the time column between steps')
+
     args = parser.parse_args()
 
     main(args)
