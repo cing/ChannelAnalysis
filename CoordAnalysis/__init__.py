c187942768b3a8d39cfbad7be91ea1507e43b044
Christopher Ing <ing.chris@gmail.com>
2013-07-30 01:10:01 -0400

    No code change, just more general naming

--- CoordAnalysis/__init__.py
+++ CoordAnalysis/__init__.py
@@ -3,6 +3,6 @@ from Ion_Preprocessor import *
 from Occupancy_Vs_Time import *
 from Coordination_Histograms import *
 from Coordination_Plots import *
-from Regex_Grouping import *
+from State_Grouping import *
 from Regex_Histograms import *
 from Regex_Transitions import *
